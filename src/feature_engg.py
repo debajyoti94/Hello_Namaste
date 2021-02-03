205b1c885b6b08cca199fe86bdecb32a944663ea
debajyoti <debajyoti94@gmail.com>
2021-02-03 15:37:19 +0530

    Added functionality for dumping and loading pickled files.

--- src/feature_engg.py
+++ src/feature_engg.py
@@ -1,16 +1,68 @@
 ''' Here we apply data preprocessing techniques on the raw dataset '''
 
 # import modules here
+import config
 
+import abc
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+import pickle
+
+from matplotlib import pyplot as plt
 
 # create abc class here
+class MustHaveForDP:
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def cleaning_data(self):
+        '''
+        In this function we take the input data and transform it to a
+         format that can be used for the model to train
+        :return:
+        '''
+        return
+
 
 # create data preprocessing class here
+class DataPreprocessing(MustHaveForDP):
+
+    def cleaning_data(self, dataset_path):
+        # tokenize the text here
+
+        # create input and output sequences
+
+        return
 
 
-# tokenize the text here
+    def create_embedding_matrix(self):
+        # creatng embedding matrix
+        return
 
-# create input and output sequences
+
+class DumpLoadFile:
+
+    def dump_file(self, filename, *file):
+        '''
+
+        :param filename:
+        :param file:
+        :return:
+        '''
+        with open(filename, 'wb') as pickle_handle:
+            pickle.dump(file, pickle_handle)
 
 
-# creatng embedding matrix
+    def load_file(self, filename):
+        '''
+
+        :param filename:
+        :return:
+        '''
+        with open(filename, 'rb') as pickle_handle:
+            return pickle.load(pickle_handle)
+
+
