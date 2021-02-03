1fd3406147c501ff5ccb4cf9a913fc3ffb58fee9
debajyoti <debajyoti94@gmail.com>
2021-02-03 16:00:56 +0530

    Added unit test cases for this project. Functions are declared, func body needs to be written.

--- src/test_functionalities.py
+++ src/test_functionalities.py
@@ -1,8 +1,26 @@
 ''' Here we define the unit test cases for our project'''
 
 # import modules here
-
+import config
+import pickle
 
 # define test cases here
+class TestFunctionalities:
+
+    # check if the raw file exists
+    def test_file_exists(self):
+
+        return
+
+    # test if nan exists in the embedding matrix
+    # ideally it shouldn't
+    def test_embedding_matrix(self):
+
+        return
+
+    # test if sos and eos tokens exist in the dataset
+    def test_sos_eos_tokens(self):
+
+        return
 
 
