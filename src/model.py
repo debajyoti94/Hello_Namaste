a3036f0863849226e62d44a2307a1cc30a9c333f
debajyoti <debajyoti94@gmail.com>
2021-02-03 20:51:30 +0530

    Created basic template for model training and translation stage.

--- src/model.py
+++ src/model.py
@@ -1,15 +1,22 @@
 ''' Here we create the sequence to sequence model'''
 
 # import modules here
+import config
 
+from keras.layers import Input,LSTM,Dense
+from keras.models import Model
+from keras.optimizers import Adam
 
+class TranslationModel:
 
+    def train_translation_model(self):
+        # create encoder model
 
-# create encoder model
+        # create decoder model
+        return
 
+    def get_translation(self):
 
-
-
-# create decoder model
+        return
 
 
