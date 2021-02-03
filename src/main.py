a84ca7c5099b355bcbe5f45a02365384deeb80f7
debajyoti <debajyoti94@gmail.com>
2021-02-03 16:10:02 +0530

    Added structure to the code, now it is easy to follow.

--- src/main.py
+++ src/main.py
@@ -3,15 +3,46 @@
 
 
 # import modules here
+import config
+import model
+import feature_engg
+
+import argparse
 
 
-# define commandline arguments using argparse
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    # define commandline arguments using argparse
+    parser.add_argument('--preprocess', type=str,
+                        help='Provide arguments \"--preprocess data\" '
+                             'to clean the raw data and convert it to a usable format.')
+
+    parser.add_argument('--train', type=str,
+                        help='Provide arguments \"--train nmt\" '
+                             'to train the Neural Machine Translation(NMT) model.')
+
+    parser.add_argument('--test', type=str,
+                        help='Provide arguments \"--test translation\" '
+                             'to provide input in English and get translation in Hindi.')
+
+    args = parser.parse_args()
+
+    # creating object for loading and dumping pickled files
+    dl_obj = feature_engg.DumpLoadFile()
+
+    if args.preprocess == 'data':
+
+        # call data preprocessing functions here
+        pass
+
+    elif args.train == 'nmt':
 
 
-# call data preprocessing functions here
+        # train the model here
+        pass
 
+    elif args.test == 'translation':
 
-# train the model here
-
-
-# get hindi translation here
+        # get hindi translation here
+        pass
