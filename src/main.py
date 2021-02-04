24edb3959f2968ab01f2aa6506a05f125115dfa0
debajyoti <debajyoti94@gmail.com>
2021-02-04 22:19:22 +0530

    Tested functionality for preprocessing raw data.

--- src/main.py
+++ src/main.py
@@ -34,9 +34,34 @@ if __name__ == '__main__':
     if args.preprocess == 'data':
 
         # call data preprocessing functions here
-        pass
+        fr_obj = feature_engg.DataPreprocessing()
+        input_seq_encoder, input_seq_decoder,\
+        output_seq_decoder, input_tokenizer,\
+        translation_tokenizer = fr_obj.cleaning_data(config.RAW_DATASET)
+
+        # getting fixed length padded sequences
+        padded_encoder_input_seq, \
+        padded_decoder_input_seq, \
+        padded_decoder_output_seq,\
+        max_encoder_input_seq_len, \
+        max_decoder_input_seq_len= fr_obj.get_fixed_length_sequences(input_seq_encoder, input_seq_decoder, output_seq_decoder)
+
+        dl_obj.dump_file(config.INPUT_SEQ_ENCODER, input_seq_encoder)
+        dl_obj.dump_file(config.INPUT_SEQ_DECODER, input_seq_decoder)
+        dl_obj.dump_file(config.OUTPUT_SEQ_DECODER, output_seq_decoder)
+        dl_obj.dump_file(config.INPUT_TOKENIZER, input_tokenizer)
+        dl_obj.dump_file(config.TRANSLATION_TOKENIZER, translation_tokenizer)
+
+        dl_obj.dump_file(config.PADDED_ENCODER_INPUT_SEQ, padded_encoder_input_seq)
+        dl_obj.dump_file(config.PADDED_DECODER_INPUT_SEQ, padded_decoder_input_seq)
+        dl_obj.dump_file(config.PADDED_DECODER_OUTPUT_SEQ, padded_decoder_output_seq)
+        dl_obj.dump_file(config.MAX_ENCODER_INPUT_SEQ_LEN, max_encoder_input_seq_len)
+        dl_obj.dump_file(config.MAX_DECODER_INPUT_SEQ_LEN, max_decoder_input_seq_len)
 
     elif args.train == 'nmt':
+
+        # train the model here
+
 
 
         # train the model here
